0645c6bb4132dd5509a407f52a1514d2f7cfe541
elizabethchanda27 <ichanda73@gmail.com>
2024-11-26 12:05:39 -0500

    adjusted skeleton for event cards

--- src/components/Skeletons/ScheduleSkeleton.tsx
+++ src/components/Skeletons/ScheduleSkeleton.tsx
@@ -2,24 +2,21 @@
 
 export const ScheduleSkeleton = () => {
   return (
-    <div className="overflow-hidden py-4">
-      {/* Add a container to limit the overall width */}
-      <div className="max-w-8xl mx-auto px-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {[...Array(4)].map((_, index) => (
-            <div
-              key={index}
-              className="bg-white block w-full flex flex-col justify-center items-start space-y-3 rounded-lg p-7 shadow-lg animate-pulse max-w-80"
-            >
-              {/* Skeleton Lines for Title and Details */}
-              <div className="animate-pulse h-3 bg-pink-accent rounded-md w-3/4"></div>
-
-              {/* Additional Skeleton Lines */}
-              <div className="animate-pulse h-3 bg-brown rounded-md w-1/2"></div>
-              <div className="animate-pulse h-3 bg-brown rounded-md w-5/6"></div>
-            </div>
-          ))}
-        </div>
+    <div className="min-h-screen flex items-start py-5">
+      {/* Scrollable horizontal container */}
+      <div className="flex flex-row overflow-x-auto space-x-4 max-w-full">
+        {/* Skeleton cards */}
+        {[...Array(5)].map((_, index) => (
+          <div
+            key={index}
+            className="relative bg-white border border-gray-200 rounded-lg p-5 overflow-hidden w-80 shadow-lg animate-pulse"
+          >
+            {/* Skeleton Lines for Title and Details */}
+            <div className="animate-pulse h-3 bg-pink-accent rounded-md w-3/4 mb-3"></div>
+            <div className="animate-pulse h-3 bg-brown rounded-md w-1/2 mb-3"></div>
+            <div className="animate-pulse h-3 bg-brown rounded-md w-5/6 mb-3"></div>
+          </div>
+        ))}
       </div>
     </div>
   );
