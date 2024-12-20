8d792b4675e8875af68102aad725c5f66563d817
mellieho9 <melioraho9@gmail.com>
2024-12-20 12:46:21 -0500

    added code of conduct links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,11 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 import { FooterContent } from "./FooterContent";
 import { SocialMediaBar } from "./SocialMediaBar";
 import { Link } from "../Link";
-import { link_mailinglist, social_email } from "@/utils/Urls";
+import {
+  link_mailinglist,
+  mlh_codeofconduct,
+  social_email,
+} from "@/utils/Urls";
 
 export function Footer() {
   return (
@@ -11,20 +15,14 @@ export function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-3 justify-left md:justify-center items-start gap-2 md:gap-4">
           <span className="hidden sm:block">
             <FooterContent title="Keep up with us">
-              Join {" "}
-              <Link href={link_mailinglist}>
-                our mailing list
-              </Link>
-              !
+              Join <Link href={link_mailinglist}>our mailing list</Link>!
             </FooterContent>
           </span>
           <FooterContent title="Administration">
-            Pearl Hacks abides by the MLH Code of Conduct. For questions or
-            feedback, contact us at{" "}
-            <Link href={social_email}>
-              questions@pearlhacks.com
-            </Link>
-            .
+            Pearl Hacks abides by{" "}
+            <Link href={mlh_codeofconduct}>the MLH Code of Conduct</Link>. For
+            questions or feedback, contact us at{" "}
+            <Link href={social_email}>questions@pearlhacks.com</Link>.
           </FooterContent>
           <FooterContent title="Inclusivity Statement">
             Pearl Hacks is an event designed to uplift women and gender
