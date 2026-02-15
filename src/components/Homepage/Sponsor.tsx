27037cbec1e39dd3f5f77b1ad68afeb7d53682af
Nicholas Do <khanhdo1027@gmail.com>
2026-02-15 17:38:14 -0500

    update 2026 sponsor

--- src/components/Homepage/Sponsor.tsx
+++ src/components/Homepage/Sponsor.tsx
@@ -10,7 +10,7 @@ export function Sponsor({ sponsors }: SponsorProps) {
       />
       <div className="w-full absolute top-32 sm:top-40 md:top-60 lg:top-[20rem] flex flex-col justify-center items-center text-center px-5 pb-32 md:px-10 space-y-8">
         <h2 className="text-white font-sans font-bold text-2xl mt-4">
-          Our 2025 Sponsors
+          Our 2026 Sponsors
         </h2>
         <SponsorGrid sponsors={sponsors} />
       </div>
